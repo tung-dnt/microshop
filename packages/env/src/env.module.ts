a28db63ab9fcf7e9a3414bfdb58b671c336e410a
Tien Nguyen <freedom172000@gmail.com>
2024-01-02 23:33:22 +0700

    feat: add script to link config env folder

--- packages/env/src/env.module.ts
+++ packages/env/src/env.module.ts
@@ -9,7 +9,7 @@ import type { ClassConstructor } from 'class-transformer'
 import { plainToClass } from 'class-transformer'
 import { validateSync } from 'class-validator'
 import { destr } from 'destr'
-import * as fs  from 'fs'
+import * as fs from 'fs'
 import { merge } from 'lodash'
 import * as path from 'path'
 
@@ -26,15 +26,16 @@ export class EnvModule extends ConfigurableModuleClass {
       imports: [
         ConfigModule.forRoot({
           ignoreEnvFile: true,
-          load: [() => {
-
-            return this.validate(options.class, this.load(options.env))
-          }]
-        })
+          load: [
+            () => {
+              return this.validate(options.class, this.load(options.env))
+            },
+          ],
+        }),
       ],
       // providers and exports have to be re/defined here because we're overriding the register method
-      providers: [EnvService],
-      exports: [EnvService]
+      providers: [ EnvService ],
+      exports: [ EnvService ],
     }
   }
 
@@ -49,27 +50,42 @@ export class EnvModule extends ConfigurableModuleClass {
 
   private static load(env?: string) {
     let config: Record<string, unknown> = {}
-    const [root, extension] = ['config', '.json']
-    const configs = ['global', env ? `global.${env}` : false, 'local', env ? `local.${env}` : false]
+    const [ root, extension ] = [ 'env', '.json' ]
+    const configs = [
+      'global',
+      env ? `global.${env}` : false,
+      'local',
+      env ? `local.${env}` : false,
+    ]
 
     const configFilePaths = configs
       .filter(Boolean)
       .map(String)
-      .map((name) => path.resolve(path.join(process.cwd(), root), name + extension))
+      .map((name) =>
+        path.resolve(path.join(process.cwd(), root), name + extension)
+      )
       .filter((path) => fs.existsSync(path))
 
     const readJson = (filePath: string) => {
       return destr(fs.readFileSync(filePath, 'utf-8')) ?? {}
     }
 
-    for (const configFilePath of configFilePaths) config = merge(config, readJson(configFilePath))
+    for (const configFilePath of configFilePaths)
+      config = merge(config, readJson(configFilePath))
 
     return config
   }
 
-  private static validate<T extends object>(klass: ClassConstructor<T>, config: T) {
-    const validatedConfig = plainToClass(klass, config, { enableImplicitConversion: true })
-    const errors = validateSync(validatedConfig, { skipMissingProperties: false })
+  private static validate<T extends object>(
+    klass: ClassConstructor<T>,
+    config: T
+  ) {
+    const validatedConfig = plainToClass(klass, config, {
+      enableImplicitConversion: true,
+    })
+    const errors = validateSync(validatedConfig, {
+      skipMissingProperties: false,
+    })
 
     if (errors.length > 0) throw new Error(errors.toString())
 
---
+++ tools/scripts/link-configs.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const fs = require('fs/promises')
+const glob = require('glob-promise')
+const chalk = require('chalk')
+
+const relative = (x) => path.relative(process.cwd(), x)
+const root = (x) => path.join(process.cwd(), x)
+
+async function globals() {
+  console.log('linking global configs environment...')
+  const pattern = 'global?(.*).json'
+  const configs = await glob(root(`./env/${pattern}`))
+  const projects = await glob(root('./apps/*'))
+
+  for (const project of projects) {
+    const configDir = path.join(path.join(project, './env/'))
+
+    // create /config directory if not exists
+    if (!(await fs.lstat(configDir).catch(() => null))?.isDirectory()) await fs.mkdir(configDir)
+
+    // delete all global configs
+    const existings = await glob(path.join(configDir, pattern))
+    await Promise.allSettled(existings.map((x) => fs.rm(x)))
+
+    // setup new links
+    for (const config of configs) {
+      const configPath = path.join(configDir, path.basename(config))
+      await fs.symlink(path.relative(path.dirname(configPath), config), configPath)
+      console.log(`linked ${chalk.green(relative(config))} -> ${chalk.yellow(relative(configPath))}`)
+    }
+  }
+}
+
+async function locals() {
+  console.log('linking local configs environment...')
+  const pattern = '*?(.*).json'
+  const configs = await glob(root(`./env/locals/${pattern}`)).then((configs) =>
+    configs.map((x) => {
+      const [app, env] = path
+        .basename(x)
+        .split('.')
+        .map((x) => x.trim())
+        .slice(0, -1)
+      return [x, root(`./apps/${app}/env/local${env ? '.' + env : ''}.json`)]
+    })
+  )
+
+  // delete existing configs
+  await Promise.allSettled(configs.map(([x, y]) => y).map((x) => fs.rm(x)))
+
+  for (const [config, to] of configs) {
+    try {
+      await fs.symlink(path.relative(path.dirname(to), config), to)
+      console.log(`linked ${chalk.green(relative(config))} -> ${chalk.yellow(relative(to))}`)
+    } catch (e) {
+      const message =
+        e.code === 'ENOENT'
+          ? {
+              message: `Unable to proceed with symlinking for file ${config}. Required folder ${
+                to.split('env/local.json')[0]
+              } is missing.`
+            }
+          : {}
+      console.error({...message, ...e})
+    }
+  }
+}
+
+globals().then(locals)
