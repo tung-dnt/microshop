06a2bb7aff07716046ee99b8b076d5391ac47e44
tung-dnt <tungdnt779901@gmail.com>
2024-01-31 02:52:08 +0700

    feat: update mocks production build

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,17 +1,67 @@
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import copy from 'rollup-plugin-copy';
+import { defineConfig, loadEnv, normalizePath } from 'vite';
+
+import { dependencies } from './package.json';
+
+function getRootRelativePath(subPath: string) {
+  return normalizePath(resolve(__dirname, subPath));
+}
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: { [key: string]: string[] } = {};
+  const vendorChunk: string[] = [];
+
+  // Object.keys(deps).forEach((key) => {
+  //   if (['some-library'].includes(key)) {
+  //     chunks[key] = [key];
+
+  //     return;
+  //   }
+
+  //   vendorChunk.push(key);
+  // });
+
+  chunks.vendor = vendorChunk;
+
+  return chunks;
+}
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const isMock = env.VITE_MOCK === 'true';
+
   process.env = {
     ...process.env,
     ...loadEnv(mode, process.cwd()),
   }
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      isMock &&
+        copy({
+          targets: [
+            {
+              src: getRootRelativePath('./mocks'),
+              dest: getRootRelativePath('./public'),
+            },
+          ],
+          hook: 'buildStart',
+        }),
+    ],
     server: {
       host: true, // To expose Vite server to Docker network
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            ...renderChunks(dependencies),
+          },
+        },
+      },
     },
     resolve: {
       alias: {
