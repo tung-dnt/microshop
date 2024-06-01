8042643d4a3851982ca8d46a3f0de19037729864
tung-dnt <tungdnt779901@gmail.com>
2024-06-01 09:28:00 +0700

    chore: replace old services

--- apps/gateway/src/graph-router/dto/update-graph-router.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateGraphRouterInput } from './create-graph-router.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateGraphRouterInput extends PartialType(CreateGraphRouterInput) {
-  @Field(() => Int)
-  id: number;
-}
